9a08f52ab17c0c8a4f5a5cc669884385fbff9649
Nathan <rxbrglaughlin@gmail.com>
2018-01-23 11:18:26 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,5 +3,10 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const prefix = "-";
 
+client.on("message", (message) => {
+  if(message.content == "test") {
+    message.channel.send();
+});
+
 //Bot login Token.
 client.login(process.env.BOT_TOKEN);
