03da9dc03804cada63f63e46c49df1fa83757866
Nathan <rxbrglaughlin@gmail.com>
2018-01-23 19:43:22 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,10 @@
 //Defines Bot
 const Discord = require("discord.js");
+const fs = require("fs");
 const bot = new Discord.Client();
 const prefix = "-";
+
+var books = JSON.parse(fs.readFileSync("myfile.json"));
 
 client.on("message", (message) => {
   if(message.content == "test") {
