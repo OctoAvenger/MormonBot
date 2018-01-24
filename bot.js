766b0e95ba6bc1a1fbf922ae059c4d8c81981a27
Nathan <rxbrglaughlin@gmail.com>
2018-01-23 19:45:11 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const bot = new Discord.Client();
 const prefix = "-";
 
-var BoM = JSON.parse(fs.readFileSync("book-of-mormon.json"));
+var BOM = JSON.parse(fs.readFileSync("book-of-mormon.json"));
 var DC = JSON.parse(fs.readFileSync("doctrine-and-covenants.json"));
 var PGP = JSON.parse(fs.readFileSync("pearl-of-great-price.json"));
 
