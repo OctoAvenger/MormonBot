8282b805301823a74dde8d5b86431a40c54f5e89
Nathan <rxbrglaughlin@gmail.com>
2018-03-10 14:09:59 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,35 +2,70 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client();
-const prefix = "-";
+const prefix = ">";
 
 //Pulls JSON files to main folder.
 //var nicks = JSON.parse(fs.readFileSync("nicks.json"));
-var bom = JSON.parse(fs.readFileSync("book-of-mormon.json"));
-var dc = JSON.parse(fs.readFileSync("doctrine-and-covenants.json"));
-var pgp = JSON.parse(fs.readFileSync("pearl-of-great-price.json"));
+const JSON = require("./book-of-mormon.json");
+
+function book() {
+    var out = "";
+    for(var book of json.books) {
+        for(var chapter of book.chapters) {
+            for(var verse of chapter.verses) {
+                out += "**" + verse.reference + "**\n" + verse.text + "\n";
+            }
+        }
+    }
+    return out;
+}
+function splitByLast(string, char) {
+    var index = string.lastIndexOf(char);
+    return [string.substr(0, index), string.substr(index + 1)];
+}
+
+function splitReference(ref) {
+    var split = splitByLast(ref, " ");
+    var book = split[0], chapter = split[1];
+    return {book: book, chapter: splitByLast(chapter, ":")[0], verse: splitByLast(chapter, ":")[1]};
+}
 
 //Messages and commands that MormonBot can proform.
-client.on("message", (message) => {
+bot.on("message", (message) => {
     //Help message. Useful for new users.
     if (message.content == prefix + "help") {
+        let embed = new Discord.RichEmbed();
+        embed.setColor("#FC9509");
+        embed.setTitle("My commands are as follows:\n\n");
+        embed.setDescription("**General commands:**\n" + prefix + "help *Displays this message.*\n[BOOK CHAPTER:VERSE-VERSE] Example: **1 Nephi 1:1-3** *Displays verse.*\n\n**Other commands:**\n" + prefix + "code *gives you a link to the GitHub.*\n\n**ALL COMMANDS AS OF VERSION 0.5.0**");
+        message.channel.send({ embed });
+}
+    //Verse display
+    if(message.content.startsWith(prefix)) {
+        var content = message.content.substr(prefix.length);
+        var command = message.content.split(/ (.+)/);
+        var args = command[1];
+        command = command[0];
+        if(command == "displayVerse") {
+            var spec = splitReference(args);
+            var book = json.books.filter(function(book) {
+                return book.name == spec.book;
+            });
+            var chapter = book.chapters[Number(spec.chapter) - 1];
+            var verse = chapter.verses[Number(spec.verse) - 1];
+            
             let embed = new Discord.RichEmbed();
             embed.setColor("#FC9509");
-            embed.setTitle("My commands are as follows:\n\n");
-            embed.setDescription("**General commands:**\n-help *Displays this message.*\n[BOOK CHAPTER:VERSE-VERSE] Example: **1 Nephi 1:1-3** *Displays verse.*\n\n**ALL COMMANDS AS OF VERSION 0.5.0");
+            embed.setTitle("Test\n\n");
+            embed.setDescription("**" + verse.reference + "**\n" + verse.text);
             message.channel.send({ embed });
+            console.log("Good.");
+        }
     }
-    //Code for displaying verses.
-    if (message.content.includes(bom.books.book.reference)) {
-        message.channel.send("**" + bom.books.book.reference + " - Book of Mormon (BoM)**\n\n``` <" + bom.books.book.verse + "> " + bom.books.book.text + "```");
+    if (message.content == prefix + "code") {
+        message.channel.send("https://github.com/OctoAvenger/MormonBot");
     }
-    if (message.content.includes(dc.sections.section.reference)) {
-        message.channel.send("**" + dc.sections.section.reference + " - Doctrine and Covenants (D&C)**\n\n``` <" + dc.sections.section.verse + "> " + dc.sections.section.text + "```");
-    }
-    if (message.content.includes(pgp.books.book.reference)) {
-        message.channel.send("**" + pgp.books.book.reference + " - Pearl of Great Price (PGP)**\n\n``` <" + pgp.books.book.verse + "> " + pgp.books.book.text + "```");
-    }
-});
+    });
 
 //Bot login Token.
-client.login(process.env.BOT_TOKEN);
+bot.login("TOKEN");
